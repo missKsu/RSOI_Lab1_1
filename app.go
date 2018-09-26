8eabd6aa622968a0a2cbdae8f84b87af503ef168
missKsu <ks.evdokimova.v@gmail.com>
2018-09-26 05:42:26 +0200

    app_test.go - add tests

--- app.go
+++ app.go
@@ -1,11 +1,80 @@
 package main
 
-import "github.com/go-martini/martini"
+import (
+	"net/http"
+    "log"
+    "fmt"
+	"github.com/gorilla/mux"
+	"strconv")
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main(){
-	m := martini.Classic()
-	m.Get("/",func() string{
-		return "Hello World!!!"
-		})
-	m.Run()
+	router := mux.NewRouter().StrictSlash(true)
+
+    api := router.PathPrefix("/api").Subrouter()
+    api.HandleFunc("/",Welcome).Methods("GET")
+    api.HandleFunc("/add",Add).Methods("POST")
+    api.HandleFunc("/inc",Inc).Methods("POST")
+
+    log.Fatal(http.ListenAndServe(":3000", router))
+}
+
+func Welcome(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World!!!")
+}
+
+func Add(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+    res := CheckErr(err)
+    if res!="Continue" {
+    	fmt.Fprint(w,res)
+    }else{
+    	sum:=r.Form.Get("sum")
+    	CheckErr(err)
+    	
+    	element:=r.Form.Get("element")
+    	CheckErr(err)
+
+    	if sum != "" && element != ""{
+    		var1,err := strconv.Atoi(sum)
+    		CheckErr(err)
+    		var2,err := strconv.Atoi(element)
+    		CheckErr(err)
+    		fmt.Fprint(w,var1+var2)
+    	}else{
+    		fmt.Fprint(w, "Incorrect request")	
+    	}
+    }
+}
+
+func Inc(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+    res := CheckErr(err)
+    if res!="Continue" {
+    	fmt.Fprint(w,res)
+    }else{
+    	element:=r.Form.Get("element")
+    	CheckErr(err)
+
+    	if element != ""{
+    		variable,err := strconv.Atoi(element)
+    		CheckErr(err)
+    		fmt.Fprint(w,variable+1)
+    	}else{
+    		fmt.Fprint(w, "Incorrect request")	
+    	}
+    }
+}
+
+func CheckErr(err error) string {
+	if err != nil {
+		return "Error occured: "+err.Error()
+	}else{
+		return "Continue"
+	}
 }
---
+++ app_test.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+    "testing"
+    "net/url"
+    "net/http"
+    "io/ioutil"
+    )
+
+type IncData struct{
+    element string
+    result string
+}
+
+type AddData struct{
+    sum string
+    element string
+    result string
+}
+
+func TestInc(t *testing.T) {
+    tests:=[]IncData{{"4","5"},{"2","3"},{"","Incorrect request"}}
+    for _,val := range tests{
+        urlData := url.Values{}
+        urlData.Set("element", val.element)
+        res := check("Post","http://localhost:3000/api/inc",urlData)
+
+        if res != val.result {
+            t.Errorf("%s", res)
+        }
+    }
+}
+
+func TestAdd(t *testing.T) {
+    tests:=[]AddData{{"4","5","9"},{"2","3","5"},{"","3","Incorrect request"}}
+    for _,val := range tests{
+        urlData := url.Values{}
+        urlData.Set("sum", val.sum)
+        urlData.Set("element", val.element)
+        res := check("Post","http://localhost:3000/api/add",urlData)
+
+        if res != val.result {
+            t.Errorf("%s", res)
+        }
+    }
+}
+
+func check(method string, url string, params url.Values) string {
+    var response *http.Response
+    var err error
+    if method=="Get"{
+        response, err = http.Get(url)
+    }else if method=="Post"{
+        response, err = http.PostForm(url,params)
+    }
+    checkErr(err)
+    defer response.Body.Close()
+
+    if response.StatusCode!=200{
+        return response.Status
+    }else{
+        contents, err := ioutil.ReadAll(response.Body)
+        checkErr(err)
+        return string(contents)
+    }    
+    return ""
+}
